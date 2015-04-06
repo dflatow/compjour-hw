f332692d86f75afd8c93519f98d3b20a15a755fd
daflatow <daflatow@gmail.com>
2015-04-06 12:12:30 -0700

    customize

--- opener-project/hello-world.py
+++ opener-project/hello-world.py
@@ -1,9 +1,9 @@
 print("Hello world")
 myname = "David"
-print("My name is", myname)
+print "My name is %s" % myname
 # Note: probably shouldn't put your real age here if your
 # repo is going to be posted online
 myage = 12345
-print("I am", myage, "years old")
-print("That is roughly", 42 * 365, "days")
-print("Or,", 42 * 365 * 24 * 60 * 60, "seconds" )
+print "I am %d years old" % myage
+print "That is roughly %d days" % (myage * 365)
+print "Or, %d seconds" %  (myage * 365 * 24 * 60 * 60)
