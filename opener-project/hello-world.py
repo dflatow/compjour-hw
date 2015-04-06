96f1902bb71133590a14cc5043d3effac0ce8339
daflatow <daflatow@gmail.com>
2015-04-06 11:17:41 -0700

    add hello world script

---
+++ opener-project/hello-world.py
@@ -0,0 +1,9 @@
+print("Hello world")
+myname = "David"
+print("My name is", myname)
+# Note: probably shouldn't put your real age here if your
+# repo is going to be posted online
+myage = 12345
+print("I am", myage, "years old")
+print("That is roughly", 42 * 365, "days")
+print("Or,", 42 * 365 * 24 * 60 * 60, "seconds" )
