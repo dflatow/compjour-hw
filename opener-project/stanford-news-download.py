20983912a77e294025bf7847ca94808a7adf0fae
daflatow <daflatow@gmail.com>
2015-04-07 20:47:40 -0700

    add some more hw scripts

---
+++ opener-project/stanford-news-download.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import requests
+
+# directory to hold data
+dirname = "data-hold/stuff"
+print("Downloading the news.stanford.edu homepage")
+
+# fetch the page from the URL
+response = requests.get("http://news.stanford.edu/")
+
+# get the "text" attribute of the response
+rawhtml = response.text
+print("news.stanford.edu has", len(rawhtml), "characters")
+
+# fancy string interpolation
+print("Saving the current news.stanford.edu to my %s folder" % dirname)
+
+# open a file and prepare it for writing
+with open(dirname + "/news.stanford.edu.html", "w") as f:
+	f.write(rawhtml)
---
+++ opener-project/stanford-news-topics.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import requests
+from urlparse import urljoin
+import bs4
+
+# download the news home page at:
+# http://news.stanford.edu/news/
+home_url = "http://news.stanford.edu/news/"
+home_page = requests.get(home_url).text
+
+# make some soup
+home_soup = bs4.BeautifulSoup(home_page)
+
+# get the list of topics
+topic_links = home_soup.select('.topiclist li a')
+print("There are {} topic links total".format(len(topic_links)))
+
+# for each topic link, download the page, and print the first (i.e. latest) headline
+for link in topic_links:
+
+    # we only want URLs that have /tags as part of the URL
+    if link['href'].find('/tags') > -1:
+
+        print("Tagged topic:", link.text, "at URL:", link['href'])
+        tag_url = urljoin(home_url, link['href'])
+
+        # now fetch the topic page
+        # notice how it's basically the same pattern as the first call
+        tpage = requests.get(tag_url).text
+        tsoup = bs4.BeautifulSoup(tpage)
+        heds = tsoup.select("#main-content .postcard-text")
+
+        # getting the first member of the list
+        h = heds[0]
+        hed = h.find('h3').text
+        print("   Latest headline: {}".format(hed))
+        print("")
+        
